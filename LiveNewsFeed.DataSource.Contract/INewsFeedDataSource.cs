c083dc49d6f6e65ab59c398932f325d4ee75671c
Martin Vrábel <mail@martinvrabel.com>
2021-01-27 17:48:55 +0100

    Added project for common DataSource contracts

---
+++ LiveNewsFeed.DataSource.Contract/INewsFeedDataSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using LiveNewsFeed.Models;
+
+namespace LiveNewsFeed.DataSource.Contract
+{
+    public interface INewsFeedDataSource : IDisposable
+    {
+        Task<IList<NewsArticlePost>> GetLatestPostsAsync(int count = 50);
+        
+        Task<IList<NewsArticlePost>> GetLatestPostsAsync(Category category, int count = 50);
+
+        Task<IList<NewsArticlePost>> GetLatestPostsAsync(Tag tag, int count = 50);
+
+        Task<IList<NewsArticlePost>> GetLatestImportantPostsAsync(int count = 50);
+
+        Task<IList<NewsArticlePost>> GetNewPostsSinceLastUpdateAsync();
+    }
+}
